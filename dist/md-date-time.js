01f24d78f23e68ee1740248025c1050a63d06ecc
SimeonC <Simeon@lume.co.nz>
2015-01-09 13:05:38 +1300

    chore(release): Bump Version Number

--- dist/md-date-time.js
+++ dist/md-date-time.js
@@ -2,7 +2,7 @@
 	@license md-date-time
 	@author SimeonC
 	@license 2015 MIT
-	@version 0.0.3
+	@version 0.0.4
 	
 	See README.md for requirements and use.
 */angular.module('mdDateTime', []).directive('timeDatePicker', [
